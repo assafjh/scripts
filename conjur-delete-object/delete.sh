45dd20c0706b7a46942a99babc9dddf4e4cebbe0
Assaf Hazan <assafjh@gmail.com>
2022-12-05 16:58:00 +0200

    Make delete.sh work from anywhere

--- conjur-delete-object/delete.sh
+++ conjur-delete-object/delete.sh
@@ -6,12 +6,14 @@ SUPPORTED_OBJECT_TYPES=("variable" "policy" "webservice" "host" "user" "layer" "
 
 ! [[ " ${SUPPORTED_OBJECT_TYPES[*]} " =~ " ${1} " ]] && echo "Supported object types: ${SUPPORTED_OBJECT_TYPES[*]}" && exit 1
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 export OBJECT_TYPE=$1
 export OBJECT_PATH=$2
 
-envsubst < delete.template > delete.yml
+envsubst < $SCRIPT_DIR/delete.template > $SCRIPT_DIR/delete.yml
 
 conjur whoami
-conjur policy update -b root -f delete.yml
+conjur policy update -b root -f $SCRIPT_DIR/delete.yml
 
-rm -f delete.yml
+rm -f $SCRIPT_DIR/delete.yml
