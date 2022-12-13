77f1f15a35af65c95ae89900054e8745e2f565d4
Assaf Hazan <assafjh@gmail.com>
2022-12-13 12:21:49 +0200

    added quick switch flag between cloud an onprem

--- conjur-delete-object/delete.sh
+++ conjur-delete-object/delete.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-[ $# -ne 2 ] && printf "expecting two parameters.\nSyntax: delete.sh <OBJECT_TYPE> <OBJECT_PATH>\n" && exit 1
+ccloud=/Applications/ConjurCloudCLI.app/Contents/Resources/conjur/conjur
+onprem=conjur
+
+cli=$onprem
+
+[ $# -lt 3 ] && printf "expecting three parameters. for cloud cli, add 4th param with the value \"true\"\nSyntax: delete.sh <OBJECT_TYPE> <BRANCH> <OBJECT_PATH>\n delete.sh <OBJECT_TYPE> <BRANCH> <OBJECT_PATH> true\n" && exit 1
 
 SUPPORTED_OBJECT_TYPES=("variable" "policy" "webservice" "host" "user" "layer" "group")
 
 ! [[ " ${SUPPORTED_OBJECT_TYPES[*]} " =~ " ${1} " ]] && echo "Supported object types: ${SUPPORTED_OBJECT_TYPES[*]}" && exit 1
 
+[ "$4" == "true" ] && cli=$ccloud
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 export OBJECT_TYPE=$1
-export OBJECT_PATH=$2
+export BRANCH=$2
+export OBJECT_PATH=$3
 
 envsubst < $SCRIPT_DIR/delete.template > $SCRIPT_DIR/delete.yml
 
-conjur whoami
-conjur policy update -b root -f $SCRIPT_DIR/delete.yml
+$cli whoami
+$cli policy update -b $BRANCH -f $SCRIPT_DIR/delete.yml
 
 rm -f $SCRIPT_DIR/delete.yml
+
