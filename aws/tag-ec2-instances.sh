c4502c41c5dd7053db518d89ece3bed2fb8a2124
Assaf Hazan <assafjh@gmail.com>
2023-06-21 09:51:39 +0300

    add new tags to ec2 instances

---
+++ aws/tag-ec2-instances.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Specify the tag key and value for filtering
+TAG_KEY="owner"
+TAG_VALUE="ajh"
+
+# Specify the tag key and value that needs to be added
+NEW_TAG_KEY=CA_iEC2Retain
+NEW_TAG_VALUE=active
+
+# List EC2 instances based on tag filter
+instance_ids=$(aws ec2 describe-instances --filters "Name=tag:${TAG_KEY},Values=${TAG_VALUE}" --query 'Reservations[].Instances[].InstanceId' --output text)
+
+# Add tags to the selected instances
+if [[ -n "$instance_ids" ]]; then
+    aws ec2 create-tags --resources $instance_ids --tags Key="${NEW_TAG_KEY}",Value="${NEW_TAG_VALUE}"
+    echo "Tags added to the instances: $instance_ids"
+else
+    echo "No instances found with the specified tags."
+fi
