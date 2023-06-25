bc5fc6cf15620500684f6ac6435a8224e6957a12
Cloud User <ec2-user@ip-172-16-0-128.eu-west-2.compute.internal>
2023-06-25 11:30:45 +0000

    update

--- docker-registry/deploy-docker-registry.sh
+++ docker-registry/deploy-docker-registry.sh
@@ -2,7 +2,7 @@
 docker run -d \
     --restart=always \
     --name registry \
-    -v ./certs:/certs \
+    -v $(pwd)/certs:/certs \
     -e REGISTRY_HTTP_ADDR=0.0.0.0:5443 \
     -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/server.pem \
     -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
